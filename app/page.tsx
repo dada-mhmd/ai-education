f7c54b7e3090d35016d1dab7781620f7a37b002d
dada-mhmd <moudi.ld@hotmail.com>
2026-01-10 09:59:34 +0200

    landing, hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from '@/components/ui/button';
+import CourseList from './_components/CourseList';
+import Header from './_components/Header';
+import Hero from './_components/Hero';
 
 const Home = () => {
   return (
     <div>
-      <Button>Hi</Button>
+      <Header />
+      <Hero />
+      <CourseList />
+
+      <div className='absolute -bottom-40 -left-40 h-[500px] w-[500px] bg-purple-400/20 blur-[120px] rounded-full'></div>
+      <div className='absolute top-20 left-1/3 bottom-[-200px] h-[500px] w-[500px] bg-pink-400/20 blur-[120px] rounded-full'></div>
+      <div className='absolute bottom-[-200px] left-1/3 h-[500px] w-[500px] bg-blue-400/20 blur-[120px] rounded-full'></div>
+      <div className='absolute top-[200px] left-1/2 h-[500px] w-[500px] bg-sky-400/20 blur-[120px] rounded-full'></div>
     </div>
   );
 };
---
+++ data/constants.ts
@@ -0,0 +1,35 @@
+export const QUICK_VIDEO_SUGGESTIONS = [
+  {
+    id: 1,
+    title: 'React Basics',
+    prompt: 'Generate a quick video course tutorial on React basics',
+  },
+  {
+    id: 2,
+    title: 'Python for beginners',
+    prompt: 'Generate a python video course tutorial for beginners',
+  },
+  {
+    id: 3,
+    title: 'HTML fundemantals',
+    prompt:
+      'Generate an HTML quick video course explaining tags, elements and attributes',
+  },
+  {
+    id: 4,
+    title: 'Tailwind CSS Mastery',
+    prompt:
+      'Generate a quick video course tutorial on Tailwind CSS showing all features',
+  },
+  {
+    id: 5,
+    title: 'Javascript Essentials',
+    prompt:
+      'Generate a quick video course tutorial explaining javascript basics',
+  },
+  {
+    id: 6,
+    title: 'Next.js Quick Start',
+    prompt: 'Generate a quick video course tutorial on Next.js basics',
+  },
+];
